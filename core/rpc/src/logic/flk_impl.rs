bbd664a988767ae5c7806efc4e04c9fdc06d510e
Steven Normore <snormore@gmail.com>
2024-08-05 18:08:51 -0400

    feat(rpc): get_state_{root,proof}

--- core/rpc/src/logic/flk_impl.rs
+++ core/rpc/src/logic/flk_impl.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 use std::time::Duration;
 
+use atomo::SerdeBackend;
 use fleek_crypto::{EthAddress, NodePublicKey};
 use hp_fixed::unsigned::HpUfixed;
 use jsonrpsee::core::{RpcResult, SubscriptionResult};
 use jsonrpsee::{PendingSubscriptionSink, SubscriptionMessage};
+use lightning_application::app::ApplicationMerklizeProvider;
 use lightning_interfaces::prelude::*;
 use lightning_interfaces::types::{
     AccountInfo,
@@ -29,7 +31,9 @@ use lightning_interfaces::types::{
     Value,
 };
 use lightning_interfaces::PagingParams;
+use lightning_types::{StateProofKey, StateProofValue};
 use lightning_utils::application::QueryRunnerExt;
+use merklize::{MerklizeProvider, StateRootHash};
 
 use crate::api::FleekApiServer;
 use crate::error::RPCError;
@@ -394,6 +398,36 @@ impl<C: Collection> FleekApiServer for FleekApi<C> {
             .enqueue(tx)
             .await
             .map_err(|e| RPCError::socket(e.to_string()))?)
+    }
+
+    async fn get_state_root(&self, epoch: Option<u64>) -> RpcResult<StateRootHash> {
+        Ok(self
+            .data
+            .query_runner(epoch)
+            .await?
+            .get_state_root()
+            .map_err(|e| RPCError::custom(e.to_string()))?)
+    }
+
+    async fn get_state_proof(
+        &self,
+        key: StateProofKey,
+        epoch: Option<u64>,
+    ) -> RpcResult<(
+        Option<StateProofValue>,
+        <ApplicationMerklizeProvider as MerklizeProvider>::Proof,
+    )> {
+        let (value, proof) = self
+            .data
+            .query_runner(epoch)
+            .await?
+            .get_state_proof(key)
+            .map_err(|e| RPCError::custom(e.to_string()))?;
+        type S = <ApplicationMerklizeProvider as MerklizeProvider>::Serde;
+        // This extra serialization/deserialization is ideally not needed, but it's a workaround
+        // for the generics of the returned proof type.
+        let proof = S::deserialize(&S::serialize(&proof));
+        Ok((value, proof))
     }
 
     async fn put(&self, data: Vec<u8>) -> RpcResult<Blake3Hash> {
