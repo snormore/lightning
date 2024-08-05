89e45bd533043c70aae809ec63272ccec8023e22
Steven Normore <snormore@gmail.com>
2024-08-05 16:22:49 -0400

    feat(merklize): add lib/merklize crate

---
+++ lib/merklize/src/providers/jmt/mod.rs
@@ -0,0 +1,8 @@
+mod context;
+mod hasher;
+mod proof;
+mod provider;
+
+pub use context::JmtMerklizeContext;
+pub use proof::JmtStateProof;
+pub use provider::JmtMerklizeProvider;
---
+++ lib/merklize/src/providers/mpt/mod.rs
@@ -0,0 +1,10 @@
+mod adapter;
+mod context;
+mod hasher;
+mod layout;
+mod proof;
+mod provider;
+
+pub use context::MptMerklizeContext;
+pub use proof::MptStateProof;
+pub use provider::MptMerklizeProvider;
