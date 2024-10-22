e31aff77bd3327e93063e5b0d04d30392eea242f
Steven Normore <snormore@gmail.com>
2024-10-22 09:54:18 -0400

    chore(consensus): tests with real consensus

---
+++ core/consensus/tests/consensus.rs
@@ -0,0 +1,86 @@
+use std::time::Duration;
+
+use lightning_interfaces::prelude::*;
+use lightning_test_utils::e2e::{TestFullNodeComponentsWithRealConsensus, TestNetwork};
+use lightning_utils::application::QueryRunnerExt;
+use lightning_utils::poll::{poll_until, PollUntilError};
+use types::{ExecutionData, TransactionResponse, UpdateMethod};
+
+#[tokio::test]
+async fn test_execute_transaction() {
+    let mut network = TestNetwork::builder()
+        .with_real_consensus()
+        .with_committee_nodes::<TestFullNodeComponentsWithRealConsensus>(4)
+        .await
+        .build()
+        .await
+        .unwrap();
+
+    // Execute an `IncrementNonce` transaction from the first node.
+    let (_, receipt) = network
+        .node(0)
+        .execute_transaction_from_node(UpdateMethod::IncrementNonce {}, None)
+        .await
+        .unwrap()
+        .as_receipt();
+    assert_eq!(
+        receipt.response,
+        TransactionResponse::Success(ExecutionData::None)
+    );
+
+    // Check that the node nonce was incremented across the network.
+    poll_until(
+        || async {
+            network
+                .nodes()
+                .all(|node| node.app_query().get_node_info(&0, |node| node.nonce) == Some(1))
+                .then_some(())
+                .ok_or(PollUntilError::ConditionNotSatisfied)
+        },
+        Duration::from_secs(3),
+        Duration::from_millis(100),
+    )
+    .await
+    .unwrap();
+
+    // Shutdown the network.
+    network.shutdown().await;
+}
+
+#[tokio::test]
+async fn test_epoch_change() {
+    let mut network = TestNetwork::builder()
+        .with_real_consensus()
+        .with_genesis_mutator(|genesis| {
+            genesis.epoch_start = 0;
+            genesis.epoch_time = 120000;
+        })
+        .with_committee_nodes::<TestFullNodeComponentsWithRealConsensus>(4)
+        .await
+        .build()
+        .await
+        .unwrap();
+
+    // Check that the current epoch is 0 across the network.
+    for node in network.nodes() {
+        assert_eq!(node.app_query().get_current_epoch(), 0);
+    }
+
+    // Wait for epoch to be incremented across the network.
+    poll_until(
+        || async {
+            network
+                .nodes()
+                .all(|node| node.app_query().get_current_epoch() == 1)
+                .then_some(())
+                .ok_or(PollUntilError::ConditionNotSatisfied)
+        },
+        Duration::from_secs(20),
+        Duration::from_millis(100),
+    )
+    .await
+    .unwrap();
+
+    // Shutdown the network.
+    network.shutdown().await;
+}
